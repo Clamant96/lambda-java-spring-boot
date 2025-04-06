55707befc2843e45a125446a9b7daf1ccd105125
Kevin Alec Neri Lazzarotto <kevin.lazzarotto@hotmail.com>
2025-04-06 20:51:23 -0300

    Atualizacao de projeto para integracao com banco e repository

--- src/main/java/br/com/helpconnect/youtube/Application.java
+++ src/main/java/br/com/helpconnect/youtube/Application.java
@@ -2,7 +2,9 @@ package br.com.helpconnect.youtube;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import br.com.helpconnect.youtube.controller.YoutubeController;
 
@@ -11,6 +13,8 @@ import br.com.helpconnect.youtube.controller.YoutubeController;
 // We use direct @Import instead of @ComponentScan to speed up cold starts
 // @ComponentScan(basePackages = "br.com.helpconnect.youtube.controller")
 @Import({ YoutubeController.class })
+@EntityScan(basePackages = {"br.com.helpconnect.models.model"})
+@EnableJpaRepositories(basePackages = {"br.com.helpconnect.repositorys.repository"})
 public class Application {
 
     public static void main(String[] args) {
